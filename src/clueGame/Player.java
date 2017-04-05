aeffa14edb0aaa2cc4f51eea13b3402aa77785a7
Jake Meister <jmeister@mines.edu>
2017-04-04 22:38:10 -0600

    Cleaned up the Player.java file

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -24,59 +24,78 @@ public class Player {
 		seenWeapons = new ArrayList<Card>();
 		hand = new ArrayList<Card>();
 	}
+	
 	public Card disproveSuggestion(Solution suggestion){
 		ArrayList<Card> similarCards = new ArrayList<Card>();
+		
+		// find all similar cards between the player's hand and the suggestion
 		for (Card card : hand){
 			if (suggestion.person.equals(card.getCardName())) similarCards.add(card);
 			if (suggestion.room.equals(card.getCardName())) similarCards.add(card);
 			if (suggestion.weapon.equals(card.getCardName())) similarCards.add(card);
 		}
+		
 		if (similarCards.size() != 0){
+			// pick a random card from the player's hand if more than one matches the suggestion
 			Card returnMe = similarCards.get(new Random().nextInt(similarCards.size()));
 			return returnMe;
 		}
 		return null;
 	}
-	public void addToHand(Card card){
-		hand.add(card);
+	
+	// makes a dumb accusation
+	public Solution accuse(String person, String room, String weapon){
+		Solution solution = new Solution(person, room, weapon);
+		return solution;
 	}
+	
+	// the three methods below are for the computer player to make smart suggestions
 	public void addToSeenPeople(Card card){
 		seenPeople.add(card);
-	}
-	public void addToSeenRooms(Card card){
-		seenRooms.add(card);
-	}
+	}	
 	public void addToSeenWeapons(Card card){
 		seenWeapons.add(card);
 	}
+	// might not be necessary because a player can only suggest the room they are in
+	public void addToSeenRooms(Card card){ 
+		seenRooms.add(card);
+	}
+	
+	public void addToHand(Card card){
+		hand.add(card);
+	}
+	
 	public void clearHand(){
 		hand.clear();
 	}
+	
 	public ArrayList<Card> getHand() {
 		return hand;
 	}
+	
 	public String getPlayerName() {
 		return playerName;
 	}
+	
 	public int getRow() {
 		return row;
 	}
+	
 	public int getColumn() {
 		return column;
 	}
+	
 	public Color getColor() {
 		return color;
 	}
+	
 	public void setLocation(BoardCell newLocation){
 		row = newLocation.getRow();
 		column = newLocation.getCol();
 	}
+	
 	@Override
 	public String toString() {
-		return "Player [playerName=" + playerName + ", row=" + row + ", column=" + column + ", color=" + color + "]";
-	}
-	public Solution accuse(String person, String room, String weapon){
-		Solution solution = new Solution(person, room, weapon);
-		return solution;
+		return "The " + color + " player, " + playerName + ", is at " + row + ", " + column;
 	}
 }
